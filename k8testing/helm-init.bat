5bb147236224548df9f6a5ae9aef5d46aea04291
Iskander <iskander.sierra@gmail.com>
2018-07-25 09:57:21 +0200

    WIP

--- k8testing/helm-init.bat
+++ k8testing/helm-init.bat
@@ -1,3 +1,4 @@
 kubectl apply -f helm-rbac-config.yaml
 helm init --service-account tiller
 helm repo add incubator http://storage.googleapis.com/kubernetes-charts-incubator
+helm repo add bitnami https://charts.bitnami.com/bitnami
